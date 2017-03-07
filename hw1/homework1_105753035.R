c4907295f73518d7378e4f25c5dd3da819b845c9
Lan105753035 <cv120345@gmail.com>
2017-03-07 17:22:19 +0800

    Update homework1_105753035.R

--- hw1/homework1_105753035.R
+++ hw1/homework1_105753035.R
@@ -22,11 +22,15 @@ if (length(args)==0) {
     i<-i+1
   }
 }
-sampleTable <- read.csv(i_f,row.names=1)
-set<-file_path_sans_ext(i_f)
-weight<-max(sampleTable$weight)
-weight<-round(weight,digits = 2)
-height<-max(sampleTable$height)
-height<-round(height,digits = 2)
-x<-data.frame(set,weight,height)
-write.csv(x,file = o_f,row.names = F)
+
+  sampleTable <- read.csv(i_f,row.names=1)
+  set <- file_path_sans_ext(i_f)
+
+  weight <- max(sampleTable$weight)
+  weight <- round(weight,digits = 2)
+  height <- max(sampleTable$height)
+  height <- round(height,digits = 2)
+
+
+  x <- data.frame(set,weight,height)
+  write.csv(x,file = o_f,row.names = F)
